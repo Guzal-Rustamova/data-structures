ca5b374a3ee1e0b2312f309f7f0f58dee511305a
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2023-09-21 13:30:41 -0500

    set notes and pair program

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
 
 
 /**
@@ -11,6 +14,23 @@ public class Sieve
         Scanner in = new Scanner(System.in);
         System.out.println("Compute primes up to which integer?");
         int n = in.nextInt();
+
+        Set <Integer> numbers = new HashSet<>(); 
+        for(int x = 2; x <n+1; x++)
+        {
+            numbers.add(x); 
+        }
+
+        Iterator <Integer> iterator = numbers.iterator(); 
+        for(int divNum = 2; divNum < n+1; divNum++)
+        {
+            int num = iterator.next(); 
+            if (num%divNum==0)
+            {
+                
+            }
+        }
+
 
         // Your work goes here
         . . .
