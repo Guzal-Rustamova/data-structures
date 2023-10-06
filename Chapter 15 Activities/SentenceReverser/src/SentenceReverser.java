5e202b0523432fc5d6cfea1c547430586b5d6118
Guzal-Rustamova <111534805+Guzal-Rustamova@users.noreply.github.com>
2023-10-06 12:29:09 -0500

    finished sentence reverser

--- Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
+++ Chapter 15 Activities/SentenceReverser/src/SentenceReverser.java
@@ -17,7 +17,39 @@ public class SentenceReverser
     	Scanner scanner = new Scanner(sentence);
     	
         // Complete this method. Use a Stack.
-        ...
+        Stack <String> sentences = new Stack<>();
+        String reverse = "";
+        while(scanner.hasNext())
+        {
+            sentences.push(scanner.next());
+            if (sentences.peek().charAt(sentences.peek().length()-1) == '.'){
+                String holder = sentences.peek().substring(0,sentences.pop().length()-1); 
+                sentences.push(holder);
+                Integer n = sentences.size(); 
+                for(int i = sentences.size(); i > 0; i--){
+    
+                    if (i == 1)
+                    {
+                        reverse += sentences.pop().toLowerCase();
+                    }
+                    else if(i == n){
+                        reverse += sentences.peek().substring(0,1).toUpperCase()+sentences.pop().toLowerCase().substring(1)+" ";
+                    }
+                    else{
+                        reverse += sentences.pop().toLowerCase() + " ";
+                    }
+                    
+                }
+                reverse +=". ";
+                
+            }
+            {
+                
+            }
+        }
+
+        return reverse;
+
 
 
 
